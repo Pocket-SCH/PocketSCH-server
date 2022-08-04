9169a429c02487a322478b7b03b61f0af16dda6d
한태규 <gksxorb147@gmail.com>
2022-08-04 16:37:08 +0900

    Revert "Revert "Build : API info keyword complote (#001)""
    This reverts commit 93f289c713dedd6101929c887a719009744492c3.

--- src/main/java/gdscsch/PocketSCHserver/info/service/InfoService.java
+++ src/main/java/gdscsch/PocketSCHserver/info/service/InfoService.java
@@ -1,25 +1,65 @@
 package gdscsch.PocketSCHserver.info.service;
 
-import gdscsch.PocketSCHserver.food.entity.FoodMenu;
+import gdscsch.PocketSCHserver.info.dto.KeywordDto;
 import gdscsch.PocketSCHserver.info.entity.Keyword;
+import gdscsch.PocketSCHserver.info.exception.EmptyStringException;
 import gdscsch.PocketSCHserver.info.repository.KeywordRepository;
 import gdscsch.PocketSCHserver.token.repository.TokenRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import gdscsch.PocketSCHserver.token.entity.Token;
-import java.util.List;
+
 
 @Component
 @RequiredArgsConstructor
 public class InfoService {
+
     private final TokenRepository tokenRepository;
     private final KeywordRepository keywordRepository;
 
-    public Keyword addKeyword (String token, String keywordStr) {
-        Token currToken = tokenRepository.findByToken(token).get();
+    @Autowired
+    private ModelMapper modelMapper;
 
-        Keyword keyword = keywordRepository.save(new Keyword(currToken, keywordStr));
+    public KeywordDto.Get createKeyword(String token, KeywordDto.Craet keyword) {
 
-        return keyword;
+        if (keyword.getKeyword().equals("")) {
+            throw new EmptyStringException("{keyword: \"\"} > keyword를 빈 문자열로  받았습니다.");
+        }
+
+        Token foundToken = tokenRepository.findByToken(token).get();
+        Keyword savedKeyword = keywordRepository.save(new Keyword(foundToken, keyword.getKeyword()));
+
+        return modelMapper.map(savedKeyword, KeywordDto.Get.class);
+    }
+
+    public List<KeywordDto.Get> readKeywords(String token) {
+
+        Token foundToken = tokenRepository.findByToken(token).get();
+        List<Keyword> keywords = keywordRepository.findAllByToken(foundToken);
+        List<KeywordDto.Get> keywordDtos = new ArrayList<KeywordDto.Get>();
+
+        for (Keyword keyword : keywords) {
+            keywordDtos.add(modelMapper.map(keyword, KeywordDto.Get.class));
+        }
+
+        return keywordDtos;
+    }
+
+    public boolean deleteKeyword(String token, Integer keywordId) {
+
+        Token foundToken = tokenRepository.findByToken(token).get();
+        List<Keyword> keywords = keywordRepository.findAllByToken(foundToken);
+
+        for (Keyword keyword : keywords) {
+            if (keyword.getId().equals(keywordId)) {
+                keywordRepository.deleteById(keywordId);
+                return true;
+            }
+        }
+        return false;
     }
 }
